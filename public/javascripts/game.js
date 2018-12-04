3f7fccf90100013c7c9a2c75b50ef10362ef7360
C4J_ <joeltijssens@gmail.com>
2018-12-04 15:53:58 +0100

    Added functionality and versatility to game.js, added submit button

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,12 +1,56 @@
 var main = function(){
-    enableColorpickButton();
+    gameOne = new game(1, true);
+    initialiseButtonActions(gameOne);
+    
 }
 
-var enableColorpickButton = function(){
+var game = function(gameId, allowDupes){
+    this.cursorState = "default";
+    this.guessingRowNumber = 9;
+    this.filledCircleCounter = 0;
+    this.id = gameId;
+    this.duplicatesAllowed = allowDupes;
+}
+
+var initialiseButtonActions = function(gameInstance){
+
+    //On color selector button press, change cursor type to image corresponding to the color picked
     $(".pickerButton").click(function(){
-        $('body').css('cursor', 'url(../images/' +this.id+ '.png), auto');
-        
-        //$('guessButton').css('cursor', 'url(../images/' +this.id+ '.png), auto');
+        if(!(this.id === "nocolor")){ 
+            $('body').css('cursor', 'url(../images/' +this.id+ '.png), auto');
+            gameInstance.cursorState = this.id;
+        } else{
+            $('body').css('cursor', 'default');
+            gameInstance.cursorState = "default";
+        } 
+    });
+
+    //On click of guessing type button, change its color to cursor color
+    $(".guessButton").click(function(){
+        if($(this).parent().attr("id") === "r"+gameInstance.guessingRowNumber){
+            if(!(gameInstance.cursorState === "default")){
+                if($(this).attr("src") === "../images/emptyCircle.png") gameInstance.filledCircleCounter += 1;
+                $(this).attr("src", "../images/" +gameInstance.cursorState+ ".png");
+            } else{
+                if(!($(this).attr("src") === "../images/emptyCircle.png")) gameInstance.filledCircleCounter -= 1;
+                $(this).attr("src", "../images/emptyCircle.png");
+            }
+        } else {
+            //@TODO: Row glows up red and the currently active row glows white
+        }
+    });
+
+    //On submitting using the submit button the callback executes and does a few things further comments below
+    $(".submitButton").click(function(){
+        //Check if all four slots in this row are filled and if duplicates are allowed and present
+        if(gameInstance.duplicatesAllowed && gameInstance.filledCircleCounter>3){
+            gameInstance.guessingRowNumber -= 1;
+            gameInstance.filledCircleCounter = 0;
+        } else if(false){
+
+        } else {
+
+        }
     });
 }
 
