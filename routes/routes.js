c74560d03cade785df950ec7b7ebf6fa5a99d8a3
C4J_ <joeltijssens@gmail.com>
2018-12-05 21:39:33 +0100

    alteration to routes

--- routes/routes.js
+++ routes/routes.js
@@ -3,16 +3,18 @@ var http = require("http");
 
 module.exports = function(app){
 
-    // app.use(function(req, res, next){
-    //     if(req.)
-    // });
-
     app.get("/", function(req, res){
         console.log("returning /");
         res.sendFile("splash.html", {root: "./public"});
     });
 
+    app.get("/game", function(req, res){
+        res.sendFile("splash.html", {root: "./public"});
+    });
+
     app.post("/game", function(req, res){
+        console.log(res);
         res.sendFile("game.html", {root: "./public"});
+        //@TODO only move player here if they used the splash screen button
     });
 }
